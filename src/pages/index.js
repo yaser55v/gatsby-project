015cc452f3402369c5e1a526c21a837729db3478
yaser55v <topyaser30@gmail.com>
2022-01-09 16:23:08 +0100

    new commit1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react"
-import { Button, Container, Flex, Heading } from "theme-ui"
+import React, { useEffect, useState } from "react"
+import { Button, Container, Flex, Heading, NavLink } from "theme-ui"
 import netlifyIdentity from "netlify-identity-widget"
+import { Link } from "gatsby"
 
-
-const props = () => {
+const Home = () => {
+  const [user, setUser] = useState()
   useEffect(() => {
     netlifyIdentity.init({})
+    netlifyIdentity.on("login", user => {
+      netlifyIdentity.close()
+      setUser(user)
+    })
+    netlifyIdentity.on("logout", () => setUser())
   })
   return (
     <Container p={4} bg="muted">
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink href="#!" p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
         <Heading as="h1">Gatsby Project</Heading>
         <Button
@@ -19,9 +38,17 @@ const props = () => {
         >
           Log In
         </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            console.log(netlifyIdentity.currentUser())
+          }}
+        >
+          Log In
+        </Button>
       </Flex>
     </Container>
   )
 }
 
-export default props
+export default Home
